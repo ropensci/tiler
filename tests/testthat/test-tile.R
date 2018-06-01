b2d28e380597a94a164cb8f0542dae6eedd6857e
leonawicz <matt.rstat@gmail.com>
2018-06-01 14:16:22 -0600

    Update file indexing. Do not test .nc files. Remove ncdf from Suggests.

--- tests/testthat/test-tile.R
+++ tests/testthat/test-tile.R
@@ -14,12 +14,13 @@ test_that("tile works on different inputs", {
 
   # Test RGB/RGBA multi-band rasters
   idx <- grep("rgb", files)
-  suppressWarnings( for(i in idx) expect_is(tile(files[i], tiles[i], "0"), "NULL") )
+  suppressWarnings(for(i in idx) expect_is(tile(files[i], tiles[i], "0"), "NULL"))
 
   files <- files[-idx]
+  tiles <- tiles[-idx]
 
   # Test rejection of file with number of layers other than 1, 3 or 4
-  r <- raster(files[2])
+  r <- raster(files[basename(files) == "map_albers.grd"])
   r <- stack(r, r)
   tmp <- file.path(tempdir(), "tmp_raster.tif")
   writeRaster(r, tmp)
@@ -28,32 +29,42 @@ test_that("tile works on different inputs", {
   unlink(c(tmp, "tmp_raster"), recursive = TRUE, force = TRUE)
 
   # test png (jpg and bmp tested elsewhere)
-  expect_is(tile(files[1], tiles[1], "0"), "NULL")
+  idx <- which(basename(files) == "map.png")
+  expect_is(tile(files[idx], tiles[idx], "0"), "NULL")
+
+  files <- files[-idx]
+  tiles <- tiles[-idx]
 
   # missing CRS
   warn <- "Projection expected but is missing. Continuing as non-geographic image."
-  idx <- c(3, 5:7, 11, 13)
+  idx <- grep("NA.grd|NA.tif", files)
   for(i in idx) expect_warning(tile(files[i], tiles[i], "0"), warn)
 
   # force CRS
-  suppressWarnings( expect_is(tile(files[3], tiles[3], "0", crs), "NULL") )
+  suppressWarnings(for(i in idx[1:2]) expect_is(tile(files[i], tiles[i], "0", crs), "NULL"))
 
   # test remaining geographic maps
-  idx <- c(2, 4, 8:10, 12)
+  idx <- which(!grepl("\\.nc$|NA", files) == TRUE)
   suppressWarnings( for(i in idx) expect_is(tile(files[i], tiles[i], "0"), "NULL") )
 
   unlink(file.path(tempdir(), "map_*"), recursive = TRUE, force = TRUE)
 
+  idx <- which(basename(files) == "map_albers.grd")
+  file <- files[idx]
+  tiles <- tiles[idx]
+
   # colors
-  expect_is(tile(files[i], tiles[i], "0", col = clrs, colNA = nacol), "NULL")
+  suppressWarnings(expect_is(tile(file, tiles, "0", col = clrs, colNA = nacol), "NULL"))
 
   # resume
-  expect_is(tile(files[i], tiles[i], "0-1", resume = TRUE), "NULL")
+  suppressWarnings(expect_is(tile(file, tiles, "0-1", resume = TRUE), "NULL"))
 
   unlink(file.path(tempdir(), "map_*"), recursive = TRUE, force = TRUE)
 
   # format, method, alpha
-  expect_is(tile(files[i], tiles[i], "0", format = "tms", method = "ngb", alpha = TRUE), "NULL")
+  suppressWarnings(
+    expect_is(tile(file, tiles, "0", format = "tms", method = "ngb", alpha = TRUE), "NULL")
+  )
 
   unlink(file.path(tempdir(), "map_*"), recursive = TRUE, force = TRUE)
 })
