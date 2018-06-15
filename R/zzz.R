e182f838141ad129dc76ffdf6e3aa40da9a3c1f6
leonawicz <matt.rstat@gmail.com>
2018-06-15 14:29:01 -0600

    Improve OSGeo4W.bat detection.

--- R/zzz.R
+++ R/zzz.R
@@ -3,8 +3,9 @@
 .onLoad <- function(lib, pkg){
   .tiler_env$opts <- list(python = "python", osgeo4w = "")
   if(.Platform$OS.type == "windows"){
-    osgeo4w <- "C:/Program Files/QGIS 3.0/OSGeo4W.bat"
-    if(file.exists(osgeo4w)) tiler_options(osgeo4w = osgeo4w)
+    osgeo4w <- paste0(c("C:/OSGeo4W64/", "C:/Program Files/QGIS 3.0/"), "OSGeo4W.bat")
+    idx <- which(file.exists(osgeo4w))
+    if(length(idx)) tiler_options(osgeo4w = osgeo4w[idx[1]])
   }
 }
 
