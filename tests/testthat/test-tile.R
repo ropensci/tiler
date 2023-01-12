bb8bfbbe1dfc0a8e86c69a1947c0320e1feadab3
Alex Chubaty <achubaty@for-cast.ca>
2023-01-12 14:42:52 -0700

    remove note re: fixed error

--- tests/testthat/test-tile.R
+++ tests/testthat/test-tile.R
@@ -55,7 +55,7 @@ test_that("tile works on different inputs", {
   )
 
   idx <- grep("NA.grd|NA.tif", files)
-  for(i in idx) expect_warning(tile(files[i], tiles[i], "0"), warn) ## syntax error !
+  for(i in idx) expect_warning(tile(files[i], tiles[i], "0"), warn)
   # These used to all produce the first warning above based on an NA-valued CRS for the prepared files.
 
   # force CRS
